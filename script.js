77a0d89c8b7ba67372d1570d79a6596f69e80d39
nasibehash <s.nasibehashtari@gmail.com>
2020-07-13 12:44:45 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//scroll to top 
+function smoothScrollTo(yPos) {
+    var step = 20;
+    if (yPos < scrollY) {
+        step *= -1;
+    }
+    if (Math.abs(yPos - scrollY) <= step) { // stop Condition
+        return;
+    }
+    window.scrollBy(0, step);
+    setTimeout(function () {
+        smoothScrollTo(yPos)
+    }, 10);
+}
